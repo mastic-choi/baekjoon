f64f604cc98782d5f318247be0518afe06374d88
CHOI JUNSOO <nalssam@hotmail.com>
2024-04-13 23:59:41 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d",&a);
+    scanf("%d", &b);
+    printf("%d\n", a*(b%10));
+    printf("%d\n", a*((b/10)%10));
+    printf("%d\n", a*(b/100));
+    printf("%d\n", a*b);
+    return 0;
+}
