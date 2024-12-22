500da9a56539f938e5dd6daa6b0cc7f19b22537d
CHOI JUNSOO <65004368+mastic-choi@users.noreply.github.com>
2024-12-22 22:52:50 +0900

    [Silver II] Title: 스택 수열, Time: 1764 ms, Memory: 133440 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,35 @@
+#백준 1874 / 스택 수열
+import sys
+
+
+def input():
+  return int(sys.stdin.readline().rstrip())
+
+
+N = input()
+stack = []
+num_list = []  # 내가 만들어야 하는 수열을 입력받는다.
+range_list = []  # 임시로 수열을 저장하는 역할
+answr = []  #'+' 혹은 '-'를 입력해둔다.
+B = True
+for _ in range(N):  # 내가 만들어야 하는 수열을 입력받는다.
+  num_list.append(input())
+count = 1
+for i in num_list:
+  num = i
+  if not ((num in range_list) or (num in stack)):
+    for k in range(count, num + 1):  # stack에 pop하기
+      range_list.append(k)
+      count = count + 1
+      answr.append('+')
+  if range_list[-1] == num:  #range_list의 마지막 값이 num과 같다면
+    stack.append(range_list.pop())
+    answr.append('-')
+  else:  # range_list의 마지막 값이 num과 다르다면
+    B = False
+    break
+if B:
+  for k in answr:
+    print(k)
+else:
+  print('NO')
