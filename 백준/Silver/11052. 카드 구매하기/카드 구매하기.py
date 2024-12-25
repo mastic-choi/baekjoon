8c3fa546030c57c48b9d45eca336056f2cf8e48e
CHOI JUNSOO <65004368+mastic-choi@users.noreply.github.com>
2024-12-25 23:02:02 +0900

    [Silver I] Title: 카드 구매하기, Time: 96 ms, Memory: 110708 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,13 @@
+#백준 11052 / 카드 구매하기
+import sys
+
+n = int(sys.stdin.readline().strip())
+price = list(map(int, sys.stdin.readline().split()))
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+  a = [0] * i
+  for k in range(i):
+    a[k] = dp[i - (k + 1)] + price[k]
+  dp[i] = max(a)
+print(dp[n])
